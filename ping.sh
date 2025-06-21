895e6664701f4a2c6682332da7c8a7707ecf0284
MatiasG19 <73800355+MatiasG19@users.noreply.github.com>
2025-06-21 12:44:24 +0200

    feat: ping (#1)
    * create ping action
    
    * Update ci.yml
    
    * update
    
    * Update ci.yml
    
    * Update ci.yml
    
    * update
    
    * Update ping.sh
    
    * Update action.yml

---
+++ ping.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+attempt=1
+while [ $attempt -le "$ATTEMPTS" ]; do
+  if curl -f "$URL"; then
+    echo
+    echo
+    echo "Ping to $URL successful!"
+    exit 0
+  else
+    echo "Attempt $attempt of $ATTEMPTS"
+    attempt=$((attempt + 1))
+    sleep 5
+  fi
+done
+
+echo
+echo "Could not reach $URL after $ATTEMPTS attempts."
+exit 1
