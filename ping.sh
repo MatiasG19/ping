433fb6039dfc1605349dc9a4635c311faa5d52e8
MatiasG19 <m.galusek@gmail.com>
2025-06-21 20:01:14 +0200

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 attempt=1
-while [ $attempt -le "$1" ]; do
-  if curl -X GET "$0"; then
+while [ $attempt -le "$2" ]; do
+  if curl -X GET "$1"; then
     echo
     echo
-    echo "Ping to $0 successful!"
+    echo "Ping to $1 successful!"
     exit 0
   else
-    echo "Attempt $attempt of $1"
+    echo "Attempt $attempt of $2"
     attempt=$((attempt + 1))
     sleep 5
   fi
 done
 
 echo
-echo "Could not reach $0 after $1 attempts."
+echo "Could not reach $1 after $2 attempts."
 exit 1
