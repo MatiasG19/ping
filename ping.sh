af3ee82332cf5045fcfab5a24e74ebad050e8619
MatiasG19 <m.galusek@gmail.com>
2025-06-21 21:00:23 +0200

    replace localhost

--- ping.sh
+++ ping.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+hostip=$(/sbin/ip route|awk '/default/ { print $3 }')
+url=$1
+attempts=$2
+url="${url//localhost/$hostip}"
+
 attempt=1
-while [ $attempt -le "$2" ]; do
-  if curl -X GET "$1"; then
+while [ $attempt -le "$attempts" ]; do
+  if curl -X GET "$url"; then
     echo
     echo
     echo "Ping to $1 successful!"
     exit 0
   else
-    echo "Attempt $attempt of $2"
+    echo "Attempt $attempt of $attempts"
     attempt=$((attempt + 1))
     sleep 5
   fi
 done
 
 echo
-echo "Could not reach $1 after $2 attempts."
+echo "Could not reach $url after $attempts attempts."
 exit 1
